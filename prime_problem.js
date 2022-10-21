ab24c2cbea43c9c41b0830cf13e6bf8cc040ada1
mdharoonhussain <haroonhussain97@gmail.com>
2022-10-21 21:24:40 +0530

    prime_number_problem

---
+++ prime_problem.js
@@ -0,0 +1,17 @@
+//  Check whether a number is Prime or not
+
+   let N = 7; 
+   count = 0;
+
+    for(let i = 0;i<=N;i++){
+      if(N %i !0 0){
+     count++;
+   }
+}
+  if(count == 2){
+   console.log(num, "is prime"):
+  } else{
+   console.log(num, "is not prime);
+  }
+
+
